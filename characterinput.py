db29e546e56951c5ad56a7d9de4d9b7e94a33f40
Ebonie-M <ebonie.mcsweaney@outlook.com>
2021-02-24 22:19:10 -0500

    feat: Add new input to print text multiple times

--- characterinput.py
+++ characterinput.py
@@ -5,8 +5,13 @@ age = int(input("Please enter your age: "))
 
 diff = tgt_age - age
 year100 = current_year + diff
+msg100 = "Congratulations! You are already 100 years old!"
 
 if age < 100:
-    print(name + ", you will 100 years old in the year " + str(year100) + "!")
+    print(name + ", you will be 100 years old in the year " + str(year100) + "!")
 else:
     print("Congratulations! You are already 100 years old!")
+
+new_num = int(input("Please enter a number between 1 and 20: "))
+
+print("Congratulations! You are already 100 years old!\n"*new_num)
