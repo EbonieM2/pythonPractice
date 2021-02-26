b93949377140e2d95fbde8873506a812ccfa32ee
Ebonie-M <ebonie.mcsweaney@outlook.com>
2021-02-25 21:38:57 -0500

    refactor: Reorder if statement for conciseness
    Also changed input messages for clarity and to include the numbers
    entered by users.

--- OddorEven.py
+++ OddorEven.py
@@ -1,17 +1,17 @@
 num = int(input("Please enter a number: "))
 
-if num % 2 == 0:
-    print("This is an even number!")
-else:
-    print("This is an odd number!")
-
 if num % 4 == 0:
-    print("This number is also a multiple of 4!")
+    print(num, " is a multiple of 4!")
+elif num % 2 == 0:
+    print(num, " is an even number!")
+else:
+    print(num, " is an odd number!")
 
-num2 = int(input("Please enter a new number: "))
-check = int(input("Please enter a check number: "))
+
+num2 = int(input("Please enter a number to check: "))
+check = int(input("Please enter a number to divide by: "))
 
 if num2 % check == 0:
-    print("Num2 is a multiple of check!")
+    print(num2, " is a multiple of ", check, "!")
 else:
-    print("Num2 cannot be evenly divided by check!")
+    print(num2, " is not a multiple of ", check, "!")
